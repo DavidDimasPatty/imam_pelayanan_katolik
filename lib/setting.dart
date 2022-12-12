6134359877d1bbb02847e9430fcd4277e3191ec2
unknown <daviddimas80@gmail.com>
2022-12-13 02:43:13 +0700

    setting page

---
+++ lib/setting.dart
@@ -0,0 +1,332 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:imam_pelayanan_katolik/history.dart';
+import 'package:imam_pelayanan_katolik/profile.dart';
+import 'DatabaseFolder/fireBase.dart';
+import 'homePage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class Settings extends StatelessWidget {
+  var names;
+  var iduser;
+  var idGereja;
+  var dataUser;
+  var data;
+
+  Settings(this.names, this.iduser, this.idGereja);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        title: Text("Setting"),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.account_circle_rounded),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Profile(names, iduser, idGereja)),
+              );
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(
+              //       builder: (context) => Settings(names, emails, iduser)),
+              // );
+            },
+          ),
+        ],
+      ),
+      body: Center(
+          child: Column(children: <Widget>[
+        ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.all(20.0),
+          children: <Widget>[
+            RaisedButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //         builder: (context) => privacySafety(
+                  //             this.name, this.email, this.idUser)));
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)),
+                elevation: 10.0,
+                padding: EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.topLeft,
+                        colors: [
+                          Colors.blueAccent,
+                          Colors.lightBlue,
+                        ]),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(
+                        maxWidth: double.maxFinite, minHeight: 50.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Privacy & Safety",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                )),
+            Padding(padding: EdgeInsets.symmetric(vertical: 14)),
+            RaisedButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //     // context,
+                  //     // MaterialPageRoute(
+                  //     //     builder: (context) => termsCondition(
+                  //     //         this.name, this.email, this.idUser)));
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)),
+                elevation: 10.0,
+                padding: EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.topLeft,
+                        colors: [
+                          Colors.blueAccent,
+                          Colors.lightBlue,
+                        ]),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(
+                        maxWidth: double.maxFinite, minHeight: 50.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Terms & Conditions",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                )),
+            Padding(padding: EdgeInsets.symmetric(vertical: 14)),
+            RaisedButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //         builder: (context) =>
+                  //             aboutus(this.name, this.email, this.idUser)));
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)),
+                elevation: 10.0,
+                padding: EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.topLeft,
+                        colors: [
+                          Colors.blueAccent,
+                          Colors.lightBlue,
+                        ]),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(
+                        maxWidth: double.maxFinite, minHeight: 50.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "About Us",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                )),
+            Padding(padding: EdgeInsets.symmetric(vertical: 14)),
+            RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)),
+                elevation: 10.0,
+                padding: EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.topLeft,
+                        colors: [
+                          Colors.blueAccent,
+                          Colors.lightBlue,
+                        ]),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(
+                        maxWidth: double.maxFinite, minHeight: 50.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Giver Rating and Review",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                )),
+            Padding(padding: EdgeInsets.symmetric(vertical: 14)),
+            RaisedButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //         builder: (context) => customerService(
+                  //             this.name, this.email, this.idUser)));
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)),
+                elevation: 10.0,
+                padding: EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.topLeft,
+                        colors: [
+                          Colors.blueAccent,
+                          Colors.lightBlue,
+                        ]),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(
+                        maxWidth: double.maxFinite, minHeight: 50.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Customer Service",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                )),
+            Padding(padding: EdgeInsets.symmetric(vertical: 14)),
+            RaisedButton(
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => Login()),
+                  // );
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)),
+                elevation: 10.0,
+                padding: EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.topLeft,
+                        colors: [
+                          Colors.blueAccent,
+                          Colors.lightBlue,
+                        ]),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(
+                        maxWidth: double.maxFinite, minHeight: 50.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Log Out",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                )),
+
+            /////////
+          ],
+        )
+      ])),
+      bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              showUnselectedLabels: true,
+              unselectedItemColor: Colors.blue,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home, color: Color.fromARGB(255, 0, 0, 0)),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.token, color: Color.fromARGB(255, 0, 0, 0)),
+                  label: "Histori",
+                )
+              ],
+              onTap: (index) {
+                if (index == 1) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => History(names, iduser, idGereja)),
+                  );
+                } else if (index == 0) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            HomePage(names, iduser, idGereja)),
+                  );
+                }
+              },
+            ),
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // floatingActionButton: new FloatingActionButton(
+      //   onPressed: () {
+      //     openCamera();
+      //   },
+      //   tooltip: 'Increment',
+      //   child: new Icon(Icons.camera_alt_rounded),
+      // ),
+    );
+  }
+}
